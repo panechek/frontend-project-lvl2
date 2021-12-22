e7199b9f980d71c217beac07004cafaf1f59d25b
Anna Merkulova <panechek@inbox.ru>
2021-12-22 10:18:15 +0300

    second

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,7 +3,10 @@
 import { Command } from 'commander/esm.mjs';
 const program = new Command();
 program
+  .arguments('<filepath1> <filepath2>')
   .version('0.0.1', '-V, --version', 'output the version number')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .option ('-f, --format, [type]',  'output format')
+
 program.parse();
 console.log(program)
