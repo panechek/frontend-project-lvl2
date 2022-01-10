926416b0553e6a618b92a90549e54fe67331599b
Anna Merkulova <panechek@inbox.ru>
2022-01-10 23:33:08 +0300

    8

--- src/compare.js
+++ src/compare.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 
 const compare = (data1, data2) => {
   const keys = _.union(Object.keys(data1), Object.keys(data2));
-  const sortKeys = keys.sort();
+  const sortKeys = _.sortBy(keys);
   const newObj = (key) => {
     if (_.has(data1, key) && _.has(data2, key)) {
       if (data1[key] === data2[key]) {
