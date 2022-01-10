85a9890ece2509af53737849b5b4a5d2d9b0d733
Anna Merkulova <panechek@inbox.ru>
2022-01-10 20:33:17 +0300

    4

--- src/parsers.js
+++ src/parsers.js
@@ -4,13 +4,15 @@ import yaml from 'js-yaml';
 import * as path from 'path';
 
 const parses = (filepath) => {
-  const format = path.extname(filepath, 'utf8');
+  const format = path.extname(filepath, 'utf8').slice(1);
   const data = fs.readFileSync(filepath, 'utf8');
   let parse;
-  if (format === '.json') {
+  if (format === 'json') {
     parse = JSON.parse;
-  } else if (format === '.yml' || format === '.yaml') {
+  } else if (format === 'yml' || format === 'yaml') {
     parse = yaml.load;
+  } else {
+    throw new Error(`Error! Type ${format} is unknown.`);
   }
   return parse(data);
 };
